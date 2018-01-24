2d2bd551fa66cf2edac50d235ef5777cff70ad96
Tsotne <tsotne24@hotmail.com>
2018-01-24 23:23:53 +0000

    Documentation and Graphics

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtdisplay = New System.Windows.Forms.TextBox
+        Me.btnone = New System.Windows.Forms.Button
+        Me.btnfour = New System.Windows.Forms.Button
+        Me.btntwo = New System.Windows.Forms.Button
+        Me.btnthree = New System.Windows.Forms.Button
+        Me.btnplus = New System.Windows.Forms.Button
+        Me.btnfive = New System.Windows.Forms.Button
+        Me.btnsix = New System.Windows.Forms.Button
+        Me.btnminus = New System.Windows.Forms.Button
+        Me.btnseven = New System.Windows.Forms.Button
+        Me.btneight = New System.Windows.Forms.Button
+        Me.btnnine = New System.Windows.Forms.Button
+        Me.btntimes = New System.Windows.Forms.Button
+        Me.btnzero = New System.Windows.Forms.Button
+        Me.btndot = New System.Windows.Forms.Button
+        Me.btnc = New System.Windows.Forms.Button
+        Me.btndiv = New System.Windows.Forms.Button
+        Me.btnequals = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'txtdisplay
+        '
+        Me.txtdisplay.Location = New System.Drawing.Point(24, 12)
+        Me.txtdisplay.Name = "txtdisplay"
+        Me.txtdisplay.Size = New System.Drawing.Size(177, 20)
+        Me.txtdisplay.TabIndex = 0
+        '
+        'btnone
+        '
+        Me.btnone.Location = New System.Drawing.Point(24, 56)
+        Me.btnone.Name = "btnone"
+        Me.btnone.Size = New System.Drawing.Size(33, 23)
+        Me.btnone.TabIndex = 2
+        Me.btnone.Text = "1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnone.UseVisualStyleBackColor = True
+        '
+        'btnfour
+        '
+        Me.btnfour.Location = New System.Drawing.Point(24, 99)
+        Me.btnfour.Name = "btnfour"
+        Me.btnfour.Size = New System.Drawing.Size(33, 23)
+        Me.btnfour.TabIndex = 3
+        Me.btnfour.Text = "4"
+        Me.btnfour.UseVisualStyleBackColor = True
+        '
+        'btntwo
+        '
+        Me.btntwo.Location = New System.Drawing.Point(79, 56)
+        Me.btntwo.Name = "btntwo"
+        Me.btntwo.Size = New System.Drawing.Size(33, 23)
+        Me.btntwo.TabIndex = 4
+        Me.btntwo.Text = "2"
+        Me.btntwo.UseVisualStyleBackColor = True
+        '
+        'btnthree
+        '
+        Me.btnthree.Location = New System.Drawing.Point(135, 56)
+        Me.btnthree.Name = "btnthree"
+        Me.btnthree.Size = New System.Drawing.Size(33, 23)
+        Me.btnthree.TabIndex = 5
+        Me.btnthree.Text = "3"
+        Me.btnthree.UseVisualStyleBackColor = True
+        '
+        'btnplus
+        '
+        Me.btnplus.Location = New System.Drawing.Point(187, 56)
+        Me.btnplus.Name = "btnplus"
+        Me.btnplus.Size = New System.Drawing.Size(33, 23)
+        Me.btnplus.TabIndex = 6
+        Me.btnplus.Text = "+"
+        Me.btnplus.UseVisualStyleBackColor = True
+        '
+        'btnfive
+        '
+        Me.btnfive.Location = New System.Drawing.Point(79, 99)
+        Me.btnfive.Name = "btnfive"
+        Me.btnfive.Size = New System.Drawing.Size(33, 23)
+        Me.btnfive.TabIndex = 7
+        Me.btnfive.Text = "5"
+        Me.btnfive.UseVisualStyleBackColor = True
+        '
+        'btnsix
+        '
+        Me.btnsix.Location = New System.Drawing.Point(135, 99)
+        Me.btnsix.Name = "btnsix"
+        Me.btnsix.Size = New System.Drawing.Size(33, 23)
+        Me.btnsix.TabIndex = 8
+        Me.btnsix.Text = "6"
+        Me.btnsix.UseVisualStyleBackColor = True
+        '
+        'btnminus
+        '
+        Me.btnminus.Location = New System.Drawing.Point(187, 99)
+        Me.btnminus.Name = "btnminus"
+        Me.btnminus.Size = New System.Drawing.Size(33, 23)
+        Me.btnminus.TabIndex = 9
+        Me.btnminus.Text = "-"
+        Me.btnminus.UseVisualStyleBackColor = True
+        '
+        'btnseven
+        '
+        Me.btnseven.Location = New System.Drawing.Point(24, 148)
+        Me.btnseven.Name = "btnseven"
+        Me.btnseven.Size = New System.Drawing.Size(33, 23)
+        Me.btnseven.TabIndex = 10
+        Me.btnseven.Text = "7"
+        Me.btnseven.UseVisualStyleBackColor = True
+        '
+        'btneight
+        '
+        Me.btneight.Location = New System.Drawing.Point(79, 148)
+        Me.btneight.Name = "btneight"
+        Me.btneight.Size = New System.Drawing.Size(33, 23)
+        Me.btneight.TabIndex = 11
+        Me.btneight.Text = "8"
+        Me.btneight.UseVisualStyleBackColor = True
+        '
+        'btnnine
+        '
+        Me.btnnine.Location = New System.Drawing.Point(135, 148)
+        Me.btnnine.Name = "btnnine"
+        Me.btnnine.Size = New System.Drawing.Size(33, 23)
+        Me.btnnine.TabIndex = 12
+        Me.btnnine.Text = "9"
+        Me.btnnine.UseVisualStyleBackColor = True
+        '
+        'btntimes
+        '
+        Me.btntimes.Location = New System.Drawing.Point(187, 148)
+        Me.btntimes.Name = "btntimes"
+        Me.btntimes.Size = New System.Drawing.Size(33, 23)
+        Me.btntimes.TabIndex = 13
+        Me.btntimes.Text = "*"
+        Me.btntimes.UseVisualStyleBackColor = True
+        '
+        'btnzero
+        '
+        Me.btnzero.Location = New System.Drawing.Point(24, 191)
+        Me.btnzero.Name = "btnzero"
+        Me.btnzero.Size = New System.Drawing.Size(33, 23)
+        Me.btnzero.TabIndex = 14
+        Me.btnzero.Text = "0"
+        Me.btnzero.UseVisualStyleBackColor = True
+        '
+        'btndot
+        '
+        Me.btndot.Location = New System.Drawing.Point(79, 191)
+        Me.btndot.Name = "btndot"
+        Me.btndot.Size = New System.Drawing.Size(33, 23)
+        Me.btndot.TabIndex = 15
+        Me.btndot.Text = "."
+        Me.btndot.UseVisualStyleBackColor = True
+        '
+        'btnc
+        '
+        Me.btnc.Location = New System.Drawing.Point(135, 191)
+        Me.btnc.Name = "btnc"
+        Me.btnc.Size = New System.Drawing.Size(33, 23)
+        Me.btnc.TabIndex = 16
+        Me.btnc.Text = "C"
+        Me.btnc.UseVisualStyleBackColor = True
+        '
+        'btndiv
+        '
+        Me.btndiv.Location = New System.Drawing.Point(187, 191)
+        Me.btndiv.Name = "btndiv"
+        Me.btndiv.Size = New System.Drawing.Size(33, 23)
+        Me.btndiv.TabIndex = 17
+        Me.btndiv.Text = "/"
+        Me.btndiv.UseVisualStyleBackColor = True
+        '
+        'btnequals
+        '
+        Me.btnequals.Location = New System.Drawing.Point(24, 234)
+        Me.btnequals.Name = "btnequals"
+        Me.btnequals.Size = New System.Drawing.Size(196, 23)
+        Me.btnequals.TabIndex = 18
+        Me.btnequals.Text = "="
+        Me.btnequals.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(244, 269)
+        Me.Controls.Add(Me.btnequals)
+        Me.Controls.Add(Me.btndiv)
+        Me.Controls.Add(Me.btnc)
+        Me.Controls.Add(Me.btndot)
+        Me.Controls.Add(Me.btnzero)
+        Me.Controls.Add(Me.btntimes)
+        Me.Controls.Add(Me.btnnine)
+        Me.Controls.Add(Me.btneight)
+        Me.Controls.Add(Me.btnseven)
+        Me.Controls.Add(Me.btnminus)
+        Me.Controls.Add(Me.btnsix)
+        Me.Controls.Add(Me.btnfive)
+        Me.Controls.Add(Me.btnplus)
+        Me.Controls.Add(Me.btnthree)
+        Me.Controls.Add(Me.btntwo)
+        Me.Controls.Add(Me.btnfour)
+        Me.Controls.Add(Me.btnone)
+        Me.Controls.Add(Me.txtdisplay)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtdisplay As System.Windows.Forms.TextBox
+    Friend WithEvents btnone As System.Windows.Forms.Button
+    Friend WithEvents btnfour As System.Windows.Forms.Button
+    Friend WithEvents btntwo As System.Windows.Forms.Button
+    Friend WithEvents btnthree As System.Windows.Forms.Button
+    Friend WithEvents btnplus As System.Windows.Forms.Button
+    Friend WithEvents btnfive As System.Windows.Forms.Button
+    Friend WithEvents btnsix As System.Windows.Forms.Button
+    Friend WithEvents btnminus As System.Windows.Forms.Button
+    Friend WithEvents btnseven As System.Windows.Forms.Button
+    Friend WithEvents btneight As System.Windows.Forms.Button
+    Friend WithEvents btnnine As System.Windows.Forms.Button
+    Friend WithEvents btntimes As System.Windows.Forms.Button
+    Friend WithEvents btnzero As System.Windows.Forms.Button
+    Friend WithEvents btndot As System.Windows.Forms.Button
+    Friend WithEvents btnc As System.Windows.Forms.Button
+    Friend WithEvents btndiv As System.Windows.Forms.Button
+    Friend WithEvents btnequals As System.Windows.Forms.Button
+
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,183 @@
+﻿Public Class Form1
+    Dim First As Decimal
+    Dim Second As Decimal
+    Dim Result As Decimal
+    Dim Operations As String
+
+
+  
+    Private Sub btnone_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnone.Click
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "1"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "1"
+
+
+        End If
+    End Sub
+
+    Private Sub btntwo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btntwo.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "2"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "2"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnthree_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnthree.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "3"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "3"
+
+
+        End If
+    End Sub
+    
+    Private Sub btnfour_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnfour.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "4"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "4"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnfive_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnfive.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "5"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "5"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnsix_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsix.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "6"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "6"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnseven_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnseven.Click
+
+        If txtdisplay.Text - "0" Then
+            txtdisplay.Text = "7"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "7"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btneight_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btneight.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "8"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "8"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnnine_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnnine.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "9"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "9"
+
+
+        End If
+    End Sub
+
+
+    Private Sub btnzero_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnzero.Click
+
+        If txtdisplay.Text = "0" Then
+            txtdisplay.Text = "0"
+        Else
+            txtdisplay.Text = txtdisplay.Text + "0"
+
+
+        End If
+    End Sub
+
+    Private Sub btnequals_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnequals.Click
+        Second = txtdisplay.Text
+        If Operations = "+" Then
+            Result = First + Second
+            txtdisplay.Text = Result
+        ElseIf Operations = "-" Then
+            Result = First - Second
+            txtdisplay.Text = Result
+        ElseIf Operations = "*" Then
+            Result = First * Second
+            txtdisplay.Text = Result
+        ElseIf Operations = "/" Then
+            Result = First / Second
+            txtdisplay.Text = Result
+
+
+        End If
+    End Sub
+
+    Private Sub btnc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnc.Click
+        txtdisplay.Text = "0"
+    End Sub
+
+    Private Sub btntimes_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btntimes.Click
+        First = txtdisplay.Text
+        txtdisplay.Text = ""
+        Operations = "*"
+    End Sub
+
+    Private Sub btnminus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnminus.Click
+
+
+        First = txtdisplay.Text
+        txtdisplay.Text = ""
+        Operations = "-"
+    End Sub
+
+    Private Sub btndiv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndiv.Click
+        First = txtdisplay.Text
+        txtdisplay.Text = ""
+        Operations = "/"
+    End Sub
+
+    Private Sub btndot_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndot.Click
+        If InStr(txtdisplay.Text, ".") = 0 Then
+            txtdisplay.Text = txtdisplay.Text + "."
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnplus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnplus.Click
+        First = txtdisplay.Text
+        txtdisplay.Text = "0"
+        Operations = "+"
+    End Sub
+End Class
